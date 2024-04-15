cd657349a8f1df0de4edffa1c762fb39d41e7511
gsack <gsackie@iu.edu>
2024-04-14 20:03:44 -0400

    Gumball mach

---
+++ src/main/java/edu/iu/habahram/GumballMachine/model/IState.java
@@ -0,0 +1,11 @@
+package edu.iu.habahram.GumballMachine.model;
+
+public interface IState {
+    TransitionResult insertQuarter();
+    TransitionResult ejectQuarter();
+    TransitionResult turnCrank();
+
+    TransitionResult dispense();
+
+    String getTheName();
+}
